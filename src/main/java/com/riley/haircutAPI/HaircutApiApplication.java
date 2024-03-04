028e2f864d0f0d1c74cb1af7f19db13c7e0c6b75
Riley McKinley <rileymckinley@Rileys-MacBook-Air.local>
2024-03-04 13:04:04 -0500

    Generated Spring Initializr folder

---
+++ src/main/java/com/riley/haircutAPI/HaircutApiApplication.java
@@ -0,0 +1,13 @@
+package com.riley.haircutAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaircutApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaircutApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riley/haircutAPI/HaircutApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riley.haircutAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaircutApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
