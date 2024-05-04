a97a6ba0e3f7b67d9f1a7b4d22c8c9984ea2df47
Riley <rileyp.mckinley@gmail.com>
2024-05-03 21:33:17 -0400

    Added swagger to project

--- src/main/java/com/riley/haircutAPI/controller/AppointmentController.java
+++ src/main/java/com/riley/haircutAPI/controller/AppointmentController.java
@@ -2,6 +2,9 @@ package com.riley.haircutAPI.controller;
 
 import com.riley.haircutAPI.entity.Appointment;
 import com.riley.haircutAPI.service.AppointmentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,21 +18,41 @@ public class AppointmentController {
     private AppointmentService appointmentService;
 
     @GetMapping("/appointments")
+    @Operation(summary = "Retrieve all appointments")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved all appointments"),
+            @ApiResponse(responseCode = "400", description = "Unsuccessfully retrieved all appointments")
+    })
     public ResponseEntity<List<Appointment>> fetchAllAppointments() {
         return ResponseEntity.ok(appointmentService.fetchAllAppointments());
     }
 
     @GetMapping("/appointments/{id}")
+    @Operation(summary = "Retrieve appointment by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved appointment"),
+            @ApiResponse(responseCode = "400", description = "Unsuccessfully retrieved appointment")
+    })
     public ResponseEntity<Appointment> fetchAppointment(@PathVariable("id") Long id) {
         return ResponseEntity.ok(appointmentService.fetchAppointment(id));
     }
 
     @PostMapping("/appointments")
+    @Operation(summary = "Create new appointment")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully created new appointment"),
+            @ApiResponse(responseCode = "400", description = "Unsuccessfully created new appointment")
+    })
     public Appointment saveAppointments(@RequestBody Appointment appointment) {
         return appointmentService.saveAppointment(appointment);
     }
 
     @DeleteMapping("/appointments/{id}")
+    @Operation(summary = "Delete appointment by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully deleted appointment"),
+            @ApiResponse(responseCode = "400", description = "Unsuccessfully deleted appointment")
+    })
     public String deleteAppointment(@PathVariable("id") Long id) {
         appointmentService.deleteAppointment(id);
         return "Appointment deleted!";
--- src/main/java/com/riley/haircutAPI/controller/ClientController.java
+++ src/main/java/com/riley/haircutAPI/controller/ClientController.java
@@ -2,6 +2,10 @@ package com.riley.haircutAPI.controller;
 
 import com.riley.haircutAPI.ResponseObjects.ClientResponse;
 import com.riley.haircutAPI.entity.Client;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +17,7 @@ import com.riley.haircutAPI.service.ClientService;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class ClientController {
 
     @Autowired
@@ -21,6 +26,11 @@ public class ClientController {
     private final Logger LOGGER = LoggerFactory.getLogger(ClientController.class);
 
     @GetMapping("/clients")
+    @Operation(summary = "Fetch all clients")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved all clients"),
+            @ApiResponse(responseCode = "400", description = "Unsuccessfully retrieved all clients")
+    })
     public ResponseEntity<ClientResponse> fetchAllClients(
             @RequestParam(value = "pageNo", defaultValue = "0", required = false) int pageNo,
             @RequestParam(value = "pageSize", defaultValue = "10", required = false) int pageSize
@@ -31,27 +41,52 @@ public class ClientController {
 
     @PostMapping("/clients")
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Create a new client")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully created new client"),
+            @ApiResponse(responseCode = "400", description = "Unsuccessfully created new client")
+    })
     public ResponseEntity<Client> saveClient(@RequestBody Client client) {
         return new ResponseEntity<>(clientService.save(client), HttpStatus.CREATED);
     }
 
     @GetMapping("/clients/{id}")
+    @Operation(summary = "Fetch client by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved client"),
+            @ApiResponse(responseCode = "400", description = "Unsuccessfully retrieved client")
+    })
     public ResponseEntity<Client> fetchClientById(@PathVariable("id") Long id) {
         return ResponseEntity.ok(clientService.fetchClient(id));
     }
 
     @DeleteMapping("/clients/{id}")
+    @Operation(summary = "Delete client by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully deleted client"),
+            @ApiResponse(responseCode = "400", description = "Unsuccessfully deleted client")
+    })
     public String deleteClient(@PathVariable("id") Long id) {
         clientService.deleteClient(id);
         return "Client account deleted!";
     }
 
     @PutMapping("/clients/{id}")
+    @Operation(summary = "Update client by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully updated client"),
+            @ApiResponse(responseCode = "400", description = "Unsuccessfully updated client")
+    })
     public ResponseEntity<Client> updateClient(@RequestBody Client client, @PathVariable("id") Long id) {
         return ResponseEntity.ok(clientService.updateClient(client, id));
     }
 
     @GetMapping("/clients/email/{email}")
+    @Operation(summary = "Retrieve client by email")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved client"),
+            @ApiResponse(responseCode = "400", description = "Unsuccessfully retrieved client")
+    })
     public ResponseEntity<Client> fetchClientByEmail(@PathVariable String email) {
         return ResponseEntity.ok(clientService.fetchClientByEmail(email));
     }
